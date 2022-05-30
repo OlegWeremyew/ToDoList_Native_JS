9d15b3591fbd5dde7bd68390eee2f742c181fcb5
olegweremyew <olegweremey1994@mail.ru>
2022-05-30 16:36:13 +0300

    broken into pieces app.js (features.js and localStorage.js)

---
+++ src/assets/js/localStorage/localStorage.js
@@ -0,0 +1,48 @@
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function(todo) {
+
+        //Create todo div
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+
+        //Create list
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todo;
+
+        CreateTodoItemInList(todoDiv, newTodo);
+        createCompletedButton(todoDiv);
+        createTrashButton(todoDiv);
+
+        //attach final Todo
+        todoList.appendChild(todoDiv);
+    });
+}
